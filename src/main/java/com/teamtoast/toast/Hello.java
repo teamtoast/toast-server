8b5d7fa0e3335c78e7c9dfb538b871e3fd085696
ndb796 <ndb796@naver.com>
2018-09-08 15:53:05 +0900

    Patched Swagger UI

---
+++ src/main/java/com/teamtoast/toast/Hello.java
@@ -0,0 +1,27 @@
+package com.teamtoast.toast;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class Hello {
+
+    private final long id;
+    private final String content;
+
+    public Hello(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty(required = true)
+    @ApiModelProperty(notes = "The name of the user", required = true)
+    public String getContent() {
+        return content;
+    }
+
+}
