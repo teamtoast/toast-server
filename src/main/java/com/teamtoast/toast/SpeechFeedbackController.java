c1b39a90bda9911fb4740dd3a2557106311801a0
ndb796 <ndb796@naver.com>
2018-11-16 22:59:36 +0900

    Update

--- src/main/java/com/teamtoast/toast/SpeechFeedbackController.java
+++ src/main/java/com/teamtoast/toast/SpeechFeedbackController.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.swagger.annotations.ApiOperation;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,27 +16,141 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
+import java.util.ArrayList;
 
 @RestController
 public class SpeechFeedbackController {
 
-    private String filePath = "C:/my upload/";
+    private String filePath = "C:/my upload/"; // 사용자의 음성 파일이 업로드 되는 곳 (테스트용)
 
-    @ApiOperation(value = "영어 음성 피드백", notes = "(프로토 타입) 타겟이 되는 영어 문장과 음성 파일을 넣으면 피드백 내용이 반환됩니다. 현재 소스코드상에서는 SpeechFeedbackController.java에서 파일 업로드 경로 설정이 필요합니다.")
-    @RequestMapping(value = "/feedback/speechFeedback", produces = {"application/json"}, method = RequestMethod.POST)
-    public String feedback(@RequestParam String text, @RequestBody MultipartFile file) {
-        return getSpeechFeedback(text, file);
+    @ApiOperation(value = "특정 대화방에서의 모든 피드백", notes = ".json과 오디오 파일만 존재하는 폴더를 지정하여 전체 피드백을 제시합니다. 예를 들어 'C:\\Toast Sample Data' 경로에 3개의 음성 파일과 3개의 음성 인식 json 파일이 있으면 알아서 이에 대한 피드백을 제공합니다.")
+    @RequestMapping(value = "/feedback/getAllFeedback", produces = {"application/json"}, method = RequestMethod.POST)
+    public String getAllFeedback(@RequestParam String path) {
+        ArrayList<String> getAllPath = getAllFiles(path);
+        for(int i = 0; i < getAllPath.size(); i++) {
+            getAllPath.set(i, path + "/" + getAllPath.get(i)); // Full Path로 저장
+        }
+        ArrayList<String> results = new ArrayList<>();
+        double totalConfidence = 0.0;
+        int confidenceCount = 0;
+        try {
+            for(int i = 0; i < getAllPath.size(); i++) {
+                String now = getAllPath.get(i);
+                int index = now.lastIndexOf(".");
+                now = now.substring(0, index) + ".json";
+                // 음성 JSON 파일 읽기
+                File file = new File(now);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String s;
+                String res = "";
+                while((s = reader.readLine()) != null) {
+                    res += s;
+                }
+                // 음성 JSON은 confidence와 text로 구성됩니다.
+                JsonNode temp = new ObjectMapper().readTree(res);
+                if(!temp.isObject()) continue;
+                ObjectNode obj = (ObjectNode) temp;
+                String text = obj.get("text").toString();
+                text = text.substring(1, text.length() - 2);
+                double confidence = Double.parseDouble(obj.get("confidence").toString());
+                // 음성 인식 Confidence 값이 80% 이상인 경우만 허용 (80% 미만인 경우 음성 오류 있을 가능성이 높음.)
+                // 아예 Path List에서 제거해버리기.
+                totalConfidence += confidence;
+                confidenceCount++;
+                if(confidence < 0.8) {
+                    getAllPath.remove(i);
+                    i--;
+                    continue;
+                }
+                results.add(text);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 해당 문장의 문법 피드백 결과를 삽입합니다.
+        FeedbackController feedbackController = new FeedbackController();
+        // 최종 결과 반환 객체입니다.
+        ObjectNode res = new ObjectMapper().createObjectNode();
+        ArrayNode grammarFeedback = new ObjectMapper().createArrayNode();
+        int grammarFaultCount = 0;
+        int spellingFaultCount = 0;
+        // 먼저 문법 검사를 할 전체 문장을 삽입합니다.
+        for(int i = 0; i < results.size(); i++) {
+            ObjectNode text = new ObjectMapper().createObjectNode();
+            text.put("Expression", results.get(i));
+            String errorCheck = feedbackController.getGrammerFeedback(results.get(i));
+            try {
+                JsonNode errorNode = new ObjectMapper().readTree(errorCheck);
+                if(errorNode != null) {
+                    if(errorNode.get("result") != null) {
+                        String error = errorNode.get("errors").toString();
+                        ArrayNode array = (ArrayNode) new ObjectMapper().readTree(error);
+                        if(array.size() > 0) ((ObjectNode) array.get(0)).remove("id"); // ID 부분 지우기 (필요 없음)
+                        // 문법 오류가 있는 경우에만 Grammar Feedback 부분에 값이 들어갑니다.
+                        if(array.get(0) != null) {
+                            text.put("Grammar Feedback", array.get(0));
+                            String type = array.get(0).get("type").toString();
+                            if(type.equals("grammar")) {
+                                grammarFaultCount++;
+                            }
+                            else if(type.equals("spelling")) {
+                                spellingFaultCount++;
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            grammarFeedback.add(text);
+        }
+        res.put("All Grammar Feedbacks", grammarFeedback);
+        ArrayList<String> poorWordList = new ArrayList<>();
+        int totalAccuracy = 0;
+        int wordCount = 0;
+        double proScore = 0.0;
+        int proScoreCount = 0;
+        try {
+            for(int i = 0; i < res.get("All Grammar Feedbacks").size(); i++) {
+                // 문법적으로 오류가 없는 문장에 한해서 발음 피드백 진행
+                if(res.get("All Grammar Feedbacks").get(i).get("Grammar Feedback") == null) {
+                    String text = res.get("All Grammar Feedbacks").get(i).get("Expression").toString();
+                    String mediaPath = getAllPath.get(i);
+                    String pronunciationFeedback = getFeedbackFromFile(text, mediaPath);
+                    ArrayNode nowFeedback = (ArrayNode) (new ObjectMapper().readTree(pronunciationFeedback).get("words"));
+                    for(int j = 0; j < nowFeedback.size(); j++) {
+                        String wordString = nowFeedback.get(j).get("word").toString();
+                        String accuarcyString = nowFeedback.get(j).get("accuracy").toString();
+                        int accuracy = Integer.parseInt(accuarcyString);
+                        if(accuracy < 85) {
+                            poorWordList.add(wordString);
+                        }
+                        totalAccuracy += accuracy;
+                        wordCount += 1;
+                    }
+                    proScore += Double.parseDouble(new ObjectMapper().readTree(pronunciationFeedback).get("totalScore").toString());
+                    proScoreCount++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ArrayNode pronunciations = new ObjectMapper().createArrayNode();
+        for(int i = 0; i < poorWordList.size(); i++) {
+            pronunciations.add(poorWordList.get(i).substring(1, poorWordList.get(i).length() - 1));
+        }
+        res.put("Poor Pronunciation", pronunciations);
+        res.put("Pronunciation Score", proScore / proScoreCount);
+        res.put("Grammar Score", ((double) results.size() - grammarFaultCount) * 100 / results.size());
+        res.put("Word Score", ((double) results.size() - spellingFaultCount) * 100 / results.size());
+        res.put("Confidence Score", (totalConfidence * 100) / confidenceCount);
+        return res.toString();
     }
-    public String getSpeechFeedback(String text, MultipartFile file) {
+
+    public String getFeedbackFromFile(String text, String path) {
         String ret = "";
         try {
-            String fileName = file.getOriginalFilename();
-            File convFile = new File(filePath + fileName);
-            convFile.createNewFile();
-            FileOutputStream fos = new FileOutputStream(convFile);
-            fos.write(file.getBytes());
-            fos.close();
-            ret = getAPIResult(text, fileName);
+            ret = getAPIResult(text, path);
             // 최종 결과 반환 객체입니다.
             ObjectNode result = new ObjectMapper().createObjectNode();
             // 각 단어마다 정확도를 체크하므로 2차원 배열을 사용합니다.
@@ -75,12 +192,37 @@ public class SpeechFeedbackController {
         return ret;
     }
 
-    public String getAPIResult(String text, String fileName) {
+    /*
+    @ApiOperation(value = "영어 음성 피드백", notes = "(프로토 타입) 타겟이 되는 영어 문장과 음성 파일을 넣으면 피드백 내용이 반환됩니다. 현재 소스코드상에서는 SpeechFeedbackController.java에서 파일 업로드 경로 설정이 필요합니다.")
+    @RequestMapping(value = "/feedback/speechFeedback", produces = {"application/json"}, method = RequestMethod.POST)
+    public String feedback(@RequestParam String text, @RequestBody MultipartFile file) {
+        return getSpeechFeedback(text, file);
+    }
+    */
+
+    public String getSpeechFeedback(String text, MultipartFile file) {
+        String ret = "";
+        try {
+            String fileName = file.getOriginalFilename();
+            File convFile = new File(filePath + fileName);
+            convFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(file.getBytes());
+            fos.close();
+            ret = getFeedbackFromFile(text, fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public String getAPIResult(String text, String path) {
         String res = "";
         try {
             String url = "http://www.speechace.co/em/indexv.php"; // 파일을 전송할 URL
             String charset = "UTF-8";
-            File binaryFile = new File(filePath + fileName);
+            // File binaryFile = new File(filePath + fileName);
+            File binaryFile = new File(path);
             String boundary = Long.toHexString(System.currentTimeMillis()); // 랜덤 값을 생성합니다.
             String CRLF = "\r\n"; // multipart/form-data에 사용될 줄바꿈 텍스트입니다.
 
@@ -133,6 +275,31 @@ public class SpeechFeedbackController {
             e.printStackTrace();
         }
         return res;
+    }
+
+    public ArrayList<String> getAllFiles(String path) {
+        ArrayList<String> results = new ArrayList<>();
+        File[] files = new File(path).listFiles();
+        for (File file : files) {
+            if(file.isFile()) {
+                /*
+                String basicFileName = file.getName();
+                int index = basicFileName.lastIndexOf(".");
+                String fileName = basicFileName.substring(0, index);
+                boolean find = false;
+                for(int i = 0; i < results.size(); i++) {
+                    if(results.get(i).equals(fileName)) {
+                        find = true;
+                    }
+                }
+                if(!find) results.add(file.getName().substring(0, index));
+                */
+                String basicFileName = file.getName();
+                if(basicFileName.contains("json")) continue;
+                results.add(basicFileName);
+            }
+        }
+        return results;
     }
 
 }
