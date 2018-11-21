325770ad387f529c65fed04e522d4f03866d5c13
마수현 <bigmsh11@gmail.com>
2018-11-21 10:35:22 +0900

    피드백 존재 방 검사 추가

--- src/main/java/com/teamtoast/toast/study/RoomController.java
+++ src/main/java/com/teamtoast/toast/study/RoomController.java
@@ -2,15 +2,25 @@ package com.teamtoast.toast.study;
 
 import com.teamtoast.toast.SocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.ArrayList;
 
 @RestController
 public class RoomController {
 
     @Autowired
+    private RoomRepository repository;
+
+    @Autowired
     private SocketHandler socketHandler;
+
+    @Value("${speech-data-path}")
+    private String speechPath;
 
     @GetMapping("/studyrooms")
     public Room.Info[] getRooms() {
@@ -20,6 +30,18 @@ public class RoomController {
     @GetMapping("/studyrooms/{categoryId}")
     public Room.Info[] getRooms(@PathVariable int categoryId) {
         return socketHandler.getRoomInfos();
+    }
+
+    @GetMapping("/studyrooms/feedbacks/{userId}")
+    public Room.Data[] getFeedbackRooms(@PathVariable long userId) {
+        ArrayList<Room.Data> list = new ArrayList<>();
+        for(Room.Data data : repository.findAll()) {
+            if(new File(speechPath + "/" + data.getId() + "/" + userId).exists()) {
+                list.add(data);
+            }
+        }
+
+        return list.toArray(new Room.Data[0]);
     }
 
 }
